0c6a44dfa34f9a842e4d4daa2cdde4d27ef6882b
SiamakSm <siamak036@gmail.com>
2026-03-10 20:08:50 +0100

    Initial React project setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Patient Dashboard</h1>
+
+      <p>Patient ID: 1024</p>
+      <p>Age: 57</p>
+      <p>Usage: 6.2h</p>
+
+    </div>
+  )
+}
+
+export default App
