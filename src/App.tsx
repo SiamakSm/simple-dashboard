cb09a718860944d163e6d1b88112380d368f40cc
SiamakSm <siamak036@gmail.com>
2026-03-10 21:53:47 +0100

    Add props to PatientInfo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { useState } from "react"
 import PatientInfo from "./components/PatientInfo"
 
 function App() {
+
+  const [usage, setUsage] = useState(6.2)
+
   return (
     <div>
       <h1>Patient Dashboard</h1>
-      <PatientInfo id={1024} age={57} usage={6.2} />
+
+      <PatientInfo id={1024} age={57} usage={usage} />
+
+      <button onClick={() => setUsage(usage + 0.5)}>
+        Increase
+      </button>
+
+      <button onClick={() => setUsage(usage - 0.5)}>
+        Decrease
+      </button>
+
     </div>
   )
 }
--- src/components/PatientInfo.tsx
+++ src/components/PatientInfo.tsx
@@ -1,19 +1,18 @@
 type PatientInfoProps = {
-    id: number,
-    age: number,
-    usage: number
+  id: number
+  age: number
+  usage: number
 }
 
-
 function PatientInfo({ id, age, usage }: PatientInfoProps) {
-    return (
-        <div>
-            <h2>Patient Information</h2>
-            <p>ID: {id}</p>
-            <p>Age: {age}</p>
-            <p>Usage: {usage} hours</p>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Patient Information</h2>
+      <p>ID: {id}</p>
+      <p>Age: {age}</p>
+      <p>Usage: {usage} hours</p>
+    </div>
+  )
 }
 
 export default PatientInfo
