65a7901f0bc774a5d1289a4a1c0a0511bf6bdf52
SiamakSm <siamak036@gmail.com>
2026-03-12 18:05:08 +0100

    Add fake API simulation with patient data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
 import './App.css'
 import { useState, useEffect } from "react"
 import PatientInfo from "./components/PatientInfo"
+import { patients } from "./data/patient"
 
 function App() {
 
-  const [usage, setUsage] = useState(0)
+  const [data, setData] = useState<any>(null)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
 
     setTimeout(() => {
-      setUsage(6)
+      setData(patients[0])
       setLoading(false)
-    }, 1000);
+    }, 1000)
 
   }, [])
 
   if (loading)
-    return <p>Loading patient data ....</p>
+    return <p>Loading patient data...</p>
 
   return (
     <div>
       <h1>Patient Dashboard</h1>
 
-      <PatientInfo id={1024} age={57} usage={usage} />
+      <PatientInfo id={data.id} age={data.age} usage={data.usage} />
 
-      <button onClick={() => setUsage(usage + 0.5)}>
-        Increase
+      <button onClick={() => setData(patients[1])}>
+        Patient 2
       </button>
-      <button onClick={() => setUsage(usage - 0.5)}>
-        Decrease
+
+      <button onClick={() => setData(patients[0])}>
+        Patient 1
       </button>
 
     </div>
---
+++ src/data/patient.ts
@@ -0,0 +1,12 @@
+export const patients = [
+    {
+        id: 2024,
+        age: 257,
+        usage: 26.2
+    },
+    {
+        id: 3024,
+        age: 357,
+        usage: 36.2
+    }
+]
